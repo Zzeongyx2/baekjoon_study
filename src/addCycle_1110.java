c3b06c4e7d89cd171c888e382bad930d97f2198a
Zzeongyx2 <jeongyy98@naver.com>
2020-01-06 19:08:57 +0900

    day5

--- src/addCycle_1110.java
+++ src/addCycle_1110.java
@@ -1,41 +1,42 @@
 import java.util.Scanner;
 
 public class addCycle_1110 {
+
     public static void main(String[] args) {
         int number;
         int[] cycle = new int[2];
         int sum;
-        int cnt=0;
+        int cnt = 0;
         int temp;
 
         Scanner scan = new Scanner(System.in);
 
         number = scan.nextInt();
 
-        if(number<10){
+        if(number<10) {
             cycle[0] = 0;
             cycle[1] = number;
         }
-        else{
+        else {
             cycle[0] = number/10;
-            cycle[0] = number%10;
+            cycle[1] = number%10;
         }
 
-        while(true){
-            sum = cycle[0]+cycle[1];
+        while(true) {
+            sum = cycle[0] + cycle[1];
             temp = (cycle[1]*10)+(sum%10);
             cnt++;
 
-            if(temp<10){
+            if(temp<10) {
                 cycle[0] = 0;
                 cycle[1] = temp;
             }
-            else{
+            else {
                 cycle[0] = temp/10;
                 cycle[1] = temp%10;
             }
 
-            if(temp==number){
+            if(temp==number) {
                 System.out.println(cnt);
                 break;
             }
